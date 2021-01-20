c7da1b90b7861549aeac394deb22e9aebcf5e423
Ekaterina <esokolova@mindthedigital.com>
2021-01-21 00:20:01 +0300

    Magnit Parser

--- parse_magnit.py
+++ parse_magnit.py
@@ -5,22 +5,23 @@ from urllib.parse import urljoin
 import pymongo
 from dotenv import load_dotenv
 
-# url ='https://magnit.ru/promo/?geo=moskva'
-# response = requests.get(url)
-# with open('magnit.html', 'w', encoding='utf8') as file:
-#     file.write(response.text)
-
-# soup = bs4.BeautifulSoup(response.text, 'lxml')
-
 
 class MagnitParcer:
+    _headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15'
+    }
+    _params = {
+        'geo': 'moskva'
+    }
+    _cookies = {'mg_geo_id': '2398'}
+
     def __init__(self, start_url, data_client):
         self.start_url = start_url
         self.data_client = data_client
         self.data_base = self.data_client["magnit_parse_db"]
 
     @staticmethod
-    def _get_responce(url, *args, **kwargs):
+    def _get_response(url, *args, **kwargs):
         # todo надо обработать ошибки запросов с сделать повторный запрос
         response = requests.get(url, *args, **kwargs)
         return response
@@ -34,21 +35,25 @@ class MagnitParcer:
             self.save(product)
 
     def parse(self, url) -> dict:
-        soup = self._get_soup(self._get_responce(url, timeout =120 ,verify=False))
-        catalog_main = soup.find('div', attrs={'class': 'catalogue__main'})
-        for proguct_tag in catalog_main.find_all('a', attrs={'class' : 'card-sale'}):
-            yield self._get_product_data(proguct_tag)
+        resp = self._get_response(url, params=self._params, headers=self._headers, cookies=self._cookies)
+        print(resp)
+        soup = self._get_soup(resp)
+        # print(soup)
+        catalog_main = soup.find('div', attrs={'class': 'сatalogue__main'})
+        # print(catalog_main)
+        for product_tag in catalog_main.find_all('a', attrs={'class': 'card-sale'}):
+            yield self._get_product_data(product_tag)
 
     @property
     def data_template(self):
         return {
-            'url' : lambda tag: urljoin(self.start_url, tag.attrs.get('href')),
-            'title': lambda tag: tag.find('div', attrs={'class' : 'card-sale__title'}).text,
+            'url': lambda tag: urljoin(self.start_url, tag.attrs.get('href')),
+            'title': lambda tag: tag.find('div', attrs={'class': 'card-sale__title'}).text,
         }
 
     def _get_product_data(self, product_tag: bs4.Tag) -> dict:
         data = {}
-        for key, pattern in self.data_template:
+        for key, pattern in self.data_template.items():
             try:
                 data[key] = pattern(product_tag)
             except AttributeError:
@@ -58,11 +63,13 @@ class MagnitParcer:
     def save(self, data):
         collection = self.data_base["magnit"]
         collection.insert_one(data)
+        pass
+
 
 if __name__ == '__main__':
     load_dotenv(".env")
     data_base_url = os.getenv('DATA_BASE_URL')
     data_client = pymongo.MongoClient(data_base_url)
-    url ='https://magnit.ru/promo/?geo=moskva'
+    url = 'https://magnit.ru/promo/'
     parser = MagnitParcer(url, data_client)
     parser.run()
