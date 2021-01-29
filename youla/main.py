7f88c9948880b61510b8047c8c5a38a78ef22ccc
Ekaterina <esokolova@mindthedigital.com>
2021-01-29 23:35:38 +0300

    auto.youla.ru - parser Scrapy - Homework

--- youla/main.py
+++ youla/main.py
@@ -1,6 +1,9 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from youla.spiders.autoyoula import AutoyoulaSpider
+import os
+import pymongo
+from dotenv import load_dotenv
 
 if __name__ == '__main__':
     crawler_settings = Settings()
@@ -8,3 +11,9 @@ if __name__ == '__main__':
     crawler_process = CrawlerProcess(settings=crawler_settings)
     crawler_process.crawl(AutoyoulaSpider)
     crawler_process.start()
+    load_dotenv(".env")
+    data_base_url = os.getenv('DATA_BASE_URL')
+    data_client = pymongo.MongoClient(data_base_url)
+    data_base = data_client["youla_parse_db"]
+
+
--- youla/spiders/autoyoula.py
+++ youla/spiders/autoyoula.py
@@ -9,21 +9,43 @@ class AutoyoulaSpider(scrapy.Spider):
     css_query = {
         'brand': 'div.ColumnItemList_container__5gTrc a.blackLink',
         'pagination': 'div.Paginator_block__2XAPy a.Paginator_button__u1e7D',
-        'ads': 'article.SerpSnippet_snippet__3O1t2 a.SerpSnippet_name__3F7Yu'
+        'ads': 'article.SerpSnippet_snippet__3O1t2 a.SerpSnippet_name__3F7Yu',
     }
 
-    data_query = {
-        'title': 'div.AdvertCard_advertTitle__1S1Ak::text',
-        'price': 'div.AdvertCard_price__3dDCr::text',
-        'description': 'div.AdvertCard_descriptionInner__KnuRi::text'
-    }
+
+
+    @staticmethod
+    def get_specs(response):
+        return {
+                spec.css('.AdvertSpecs_label__2JHnS::text').get():
+                    spec.css('.AdvertSpecs_data__xK2Qx::text').get() or spec.css('a::text').get() for spec in response.css('.AdvertSpecs_row__ljPcX')
+            }
+
+    @property
+    def data_template(self):
+        return {
+            'title': lambda response: response.css("div.AdvertCard_advertTitle__1S1Ak::text").get(),
+            'price': lambda response: response.css('div.AdvertCard_price__3dDCr::text').get(),
+            'description': lambda response: response.css('div.AdvertCard_descriptionInner__KnuRi::text').get(),
+            'specifications': lambda response: self.get_specs(response),
+            'photos': lambda response: self.get_photos(response)
+        }
+
+    def save(self, data):
+        print(data)
+        collection = self.data_base["youla"]
+        collection.insert_one(data)
+        pass
 
     @staticmethod
     def gen_task(response, list_links, callback):
         for link in list_links:
             yield response.follow(link.attrib.get('href'), callback=callback)
 
-    def parse(self, response: Response):
+    def get_photos(self, response):
+        return [pict.attrib.get('src') for pict in response.css('figure.PhotoGallery_photo__36e_r img')]
+
+    def parse(self, response: Response, **kwargs):
         yield from self.gen_task(response, response.css(self.css_query['brand']), self.brand_parse)
 
     def brand_parse(self, response: Response):
@@ -32,9 +54,12 @@ class AutoyoulaSpider(scrapy.Spider):
 
     def ads_parse(self, response: Response):
         data = {}
-        for name, query in self.data_query.items():
-            data[name] = response.css(query).get()
-        print(1)
+        for name, value in self.data_template.items():
+            try:
+                data[name] = value(response)
+            except AttributeError:
+                pass
+        return data
 
 '''
 Собрать след стуркутру и сохранить в БД Монго
