f9759c00de351ba53e6e04c20e41f139ab108ea8
Ekaterina <esokolova@mindthedigital.com>
2021-02-04 03:01:42 +0300

    scraper - hh.ru version 1

--- hh-scrapy/hhru/spiders/vacancies_hh.py
+++ hh-scrapy/hhru/spiders/vacancies_hh.py
@@ -1,5 +1,9 @@
 import scrapy
 from scrapy.http import Response
+from dotenv import load_dotenv
+import os
+import pymongo
+from dotenv import load_dotenv
 
 
 class VacanciesHhSpider(scrapy.Spider):
@@ -11,17 +15,42 @@ class VacanciesHhSpider(scrapy.Spider):
         'handle_httpstatus_list': [302]
     }
 
+
     css_query = {
-        'pagination': 'div.pager-block a.HH-Pager-Control',
-        'vacancy': 'div.vacancy-serp-item__info a.bloko-link',
-        'company': ''
+        'pagination': 'div.bloko-gap a.HH-Pager-Control',
+        'vacancy': 'div.vacancy-serp-item__info a.bloko-link'
     }
 
     data_query = {
         'vacancy_name': 'h1.bloko-header-1::text',
         'salary': 'p.vacancy-salary span.bloko-header-2::text',
-        'description': ''
+        'description': '//div[@class="g-user-content"]//text()',
+        'skills': 'div.bloko-tag-list span.bloko-tag__section_text::text',
+        'company_link': 'div.vacancy-company-wrapper a.vacancy-company-name',
+        'company_name': 'div.company-header span.company-header-title-name::text',
+        'company_site': 'div.employer-sidebar a.g-user-content',
+        'activity_areas': 'div.employer-sidebar-block p::text',
+        'company_descr': 'div.g-user-content p::text'
     }
+
+    @property
+    def get_vacancy_data(self):
+        return {
+            'vacancy_name': lambda response: response.css(self.data_query['vacancy_name']).get(),
+            'salary': lambda response: ''.join(response.css(self.data_query['salary']).getall()).replace(u'\xa0', ' '),
+            'description': lambda response: ''.join(response.xpath(self.data_query['description']).getall()),
+            'skills': lambda response: response.css(self.data_query['skills']).getall(),
+            'company_link': lambda response: f"https://hh.ru/{response.css(self.data_query['company_link']).attrib.get('href')}"
+        }
+
+    @property
+    def get_company_data(self):
+        return {
+            'company_name': lambda response: response.css(self.data_query['company_name']).get(),
+            'company_site': lambda response: response.css(self.data_query['company_site']).attrib.get('href'),
+            'activity_areas': lambda response: response.css(self.data_query['activity_areas']).getall(),
+            'company_description': lambda response: ' '.join(response.css(self.data_query['company_descr']).getall())
+        }
 
     @staticmethod
     def gen_task(response, list_links, callback):
@@ -30,19 +59,40 @@ class VacanciesHhSpider(scrapy.Spider):
 
     def parse(self, response: Response):
         yield from self.gen_task(response, response.css(self.css_query['pagination']), callback=self.parse)
+        # for page in response.css('div.pager-block a.HH-Pager-Control'):
+        #     list_links = page.attrib.get('href')
+        # yield response.follow(list_links, callback=self.brand_parse)
+        # print(1)
         yield from self.gen_task(response, response.css(self.css_query['vacancy']), callback=self.vacancy_parse)
 
     def vacancy_parse(self, response: Response):
-        print(1)
         data = {}
-        data['vacancy_name'] = response.css(self.data_query['vacancy_name']).get
-        salary = response.css(self.data_query['salary']).getall
-        data['salary'] = ''.join(salary).replace(u'\xa0', ' ')
-        data['description'] = ''
-        for name, query in self.data_query.items():
-            data[name] = response.css(query).get()
+        for name, value in self.get_vacancy_data.items():
+            data[name] = value(response)
+        print ('vacansy',data)
+        # return self.save_data(data, 'vacancies')
+        link = response.css(self.data_query['company_link']).attrib.get('href')
+        return response.follow(link, callback=self.company_parse)
 
+    def company_parse(self, response: Response):
+        data = {}
+        for name, value in self.get_company_data.items():
+            data[name] = value(response)
+        print('company', data)
+        # return self.save_data(data, 'companies')
 
-
-    def save_data(self):
+    @staticmethod
+    def save_data(data, database_name):
+        load_dotenv("\.env")
+        data_base_url = os.getenv('DATA_BASE_URL')
+        data_client = pymongo.MongoClient(data_base_url)
+        data_base = data_client["hh_parse_db"]
+        collection = data_base[database_name]
+        result = collection.insert_one(data)
+        if result.inserted_id:
+            print(f"Inserted {result.inserted_id}")
         pass
+
+    # todo обработать description [e for e in list if e.strip()]
+    # todo Проверить хрень с компаниями
+    # todo написать файлы items, loaders, pipelines
--- hh-scrapy/main.py
+++ hh-scrapy/main.py
@@ -1,15 +1,9 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 from hhru.spiders.vacancies_hh import VacanciesHhSpider
-import os
-import pymongo
-from dotenv import load_dotenv
 
 
 if __name__ == '__main__':
-    load_dotenv(".env")
-    data_base_url = os.getenv('DATA_BASE_URL')
-    data_client = pymongo.MongoClient(data_base_url)
     crawler_settings = Settings()
     crawler_settings.setmodule('hhru.settings')
     crawler_process = CrawlerProcess(settings=crawler_settings)
