eeccdd63dd0d5aac9218d061f1aa0566a21cc79e
Ekaterina <esokolova@mindthedigital.com>
2021-02-04 20:19:35 +0300

    scraper - hh.ru version 2

--- hh-scrapy/hhru/spiders/vacancies_hh.py
+++ hh-scrapy/hhru/spiders/vacancies_hh.py
@@ -10,46 +10,45 @@ class VacanciesHhSpider(scrapy.Spider):
     name = 'vacancies-hh'
     allowed_domains = ['hh.ru']
     start_urls = ['https://hh.ru/search/vacancy?schedule=remote&L_profession_id=0&area=113']
-    meta = {
-        'dont_redirect': True,
-        'handle_httpstatus_list': [302]
-    }
 
-
-    css_query = {
+    xpath_query = {
         'pagination': 'div.bloko-gap a.HH-Pager-Control',
-        'vacancy': 'div.vacancy-serp-item__info a.bloko-link'
+        'vacancy': 'div.vacancy-serp-item__info a.bloko-link',
+        'other_vacancies': '//a[@data-qa="employer-page__employer-vacancies-link"]/@href'
     }
 
     data_query = {
-        'vacancy_name': 'h1.bloko-header-1::text',
-        'salary': 'p.vacancy-salary span.bloko-header-2::text',
-        'description': '//div[@class="g-user-content"]//text()',
-        'skills': 'div.bloko-tag-list span.bloko-tag__section_text::text',
-        'company_link': 'div.vacancy-company-wrapper a.vacancy-company-name',
-        'company_name': 'div.company-header span.company-header-title-name::text',
-        'company_site': 'div.employer-sidebar a.g-user-content',
-        'activity_areas': 'div.employer-sidebar-block p::text',
-        'company_descr': 'div.g-user-content p::text'
+        'vacancy_name': '//h1[@data-qa="vacancy-title"]//text()',
+        'salary': '//p[@class="vacancy-salary"]//text()',
+        'description': '//div[@data-qa="vacancy-description"]//text()',
+        'skills': '//div[@class="bloko-tag-list"]//span[@data-qa="bloko-tag__text"]//text()',
+        'company_link': '//div[@class="vacancy-company__details"]//@href',
+        'company_name': '//div[@class="company-header"]//h1//text()',
+        'company_name2': '//div[@class="vacancy-company__details"]//span//text()',
+        'company_site': '//div//a[@data-qa="sidebar-company-site"]//@href',
+        'activity_areas': '//div[contains(@class, "employer-sidebar-content")]//p/text()',
+        'company_descr': '//div[@class="g-user-content"]//text()'
     }
 
     @property
     def get_vacancy_data(self):
         return {
-            'vacancy_name': lambda response: response.css(self.data_query['vacancy_name']).get(),
-            'salary': lambda response: ''.join(response.css(self.data_query['salary']).getall()).replace(u'\xa0', ' '),
-            'description': lambda response: ''.join(response.xpath(self.data_query['description']).getall()),
-            'skills': lambda response: response.css(self.data_query['skills']).getall(),
-            'company_link': lambda response: f"https://hh.ru/{response.css(self.data_query['company_link']).attrib.get('href')}"
+            'vacancy_name': lambda response: response.xpath(self.data_query['vacancy_name']).get(),
+            'company_name': lambda response: response.xpath(self.data_query['company_name2']).get(),
+            'salary': lambda response: self.get_text(response, 'salary').replace(u'\xa0', ' '),
+            'description': lambda response: self.get_text(response, 'description'),
+            'skills': lambda response: response.xpath(self.data_query['skills']).getall(),
+            'company_link': lambda response: f"https://hh.ru/{self.get_link(response, 'company_link')}"
         }
 
     @property
     def get_company_data(self):
         return {
-            'company_name': lambda response: response.css(self.data_query['company_name']).get(),
-            'company_site': lambda response: response.css(self.data_query['company_site']).attrib.get('href'),
-            'activity_areas': lambda response: response.css(self.data_query['activity_areas']).getall(),
-            'company_description': lambda response: ' '.join(response.css(self.data_query['company_descr']).getall())
+            'company_link': lambda response: response.url,
+            'company_name': lambda response: self.get_text(response, 'company_name').replace(u'\xa0', ' '),
+            'company_site': lambda response: response.xpath(self.data_query['company_site']).get(),
+            'activity_areas': lambda response: response.xpath(self.data_query['activity_areas']).getall(),
+            'company_description': lambda response: self.get_text(response, 'company_descr')
         }
 
     @staticmethod
@@ -57,29 +56,37 @@ class VacanciesHhSpider(scrapy.Spider):
         for link in list_links:
             yield response.follow(link.attrib.get('href'), callback=callback)
 
+    def get_text(self, response, query):
+        descr_list = response.xpath(self.data_query[query]).getall()
+        return ' '.join([e for e in descr_list if e.strip()])
+
+    def get_link(self, response, query):
+        return response.xpath(self.data_query[query]).get()
+
     def parse(self, response: Response):
-        yield from self.gen_task(response, response.css(self.css_query['pagination']), callback=self.parse)
-        # for page in response.css('div.pager-block a.HH-Pager-Control'):
-        #     list_links = page.attrib.get('href')
-        # yield response.follow(list_links, callback=self.brand_parse)
-        # print(1)
-        yield from self.gen_task(response, response.css(self.css_query['vacancy']), callback=self.vacancy_parse)
+        yield from self.gen_task(response, response.css(self.xpath_query['pagination']), callback=self.parse)
+        yield from self.gen_task(response, response.css(self.xpath_query['vacancy']), callback=self.vacancy_parse)
 
     def vacancy_parse(self, response: Response):
         data = {}
         for name, value in self.get_vacancy_data.items():
             data[name] = value(response)
-        print ('vacansy',data)
-        # return self.save_data(data, 'vacancies')
-        link = response.css(self.data_query['company_link']).attrib.get('href')
-        return response.follow(link, callback=self.company_parse)
+        print ('vacanсy', data)
+        self.save_data(data, 'vacancies')
+        yield response.follow(self.get_link(response, 'company_link'), callback=self.company_parse)
 
     def company_parse(self, response: Response):
+        print(1)
         data = {}
         for name, value in self.get_company_data.items():
             data[name] = value(response)
         print('company', data)
-        # return self.save_data(data, 'companies')
+        self.save_data(data, 'companies')
+        self.other_vacancies_parse(response)
+
+    def other_vacancies_parse(self, response):
+        if response.xpath(self.xpath_query['other_vacancies']).get():
+            yield response.follow(response.xpath(self.xpath_query['other_vacancies']).get(), callback=self.parse)
 
     @staticmethod
     def save_data(data, database_name):
@@ -93,6 +100,5 @@ class VacanciesHhSpider(scrapy.Spider):
             print(f"Inserted {result.inserted_id}")
         pass
 
-    # todo обработать description [e for e in list if e.strip()]
-    # todo Проверить хрень с компаниями
+
     # todo написать файлы items, loaders, pipelines
